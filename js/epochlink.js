62341b6d2645b63fccc105d7d816022cf418fe52
だいちまる <98202777+Daichimarukana@users.noreply.github.com>
2024-05-21 23:50:17 +0900

    Add files via upload

--- js/epochlink.js
+++ js/epochlink.js
@@ -1,6 +1,9 @@
-//EpochLink.js | Made by daichimarukana | License: NYSL
+//EpochLink.js - v1.0.1 | Made by daichimarukana | License: NYSL
 function epochlink_json(adds) {
     var request = new XMLHttpRequest();
+    if(!(adds.includes('https://') || adds.includes('HTTPS://') || adds.includes('http://') || adds.includes('HTTP://'))){
+        adds = "https://"+adds;
+    }
     try {
         request.open('GET', adds, false);
         request.send();
@@ -25,8 +28,16 @@ function epochlink_unixtime(adds) {
     var endGetDt = Date.now();
 
     if (json) {
-        var roundTripTime = (endGetDt - startGetDt) / 2 / 1000;
-        return json["unix_time"] + roundTripTime;
+        if(json["protocol"] == "EpochLink"){
+            var roundTripTime = (endGetDt - startGetDt) / 2 / 1000;
+            if(json["unix_time"] < 0){
+                console.warn("Time is negative.\nEpochLink server may be using 32bit.");
+            }
+            return Math.round(json["unix_time"] + roundTripTime);
+        }else{
+            console.error("Failed to check protocol.\nProtocol must be EpochLink.");
+            return null;
+        }
     } else {
         return null;
     }
@@ -38,8 +49,16 @@ function epochlink_unixtime_ms(adds) {
     var endGetDt = Date.now();
 
     if (json) {
-        var roundTripTime = (endGetDt - startGetDt) / 2;
-        return json["unix_time_ms"] + roundTripTime;
+        if(json["protocol"] == "EpochLink"){
+            var roundTripTime = (endGetDt - startGetDt) / 2;
+            if(json["unix_time"] < 0){
+                console.warn("Time is negative.\nEpochLink server may be using 32bit.");
+            }
+            return Math.round(json["unix_time_ms"] + roundTripTime);
+        }else{
+            console.error("Failed to check protocol.\nProtocol must be EpochLink.");
+            return null;
+        }
     } else {
         return null;
     }
